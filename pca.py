ee521daba6f02ea6f9aaf099593f2a270355a977
jinze1994 <baijinze1994@163.com>
2016-12-06 18:49:12 +0800

    add pca method: no improve, even worse

--- pca.py
+++ pca.py
@@ -1,13 +1,23 @@
 #! /usr/bin/python
+import numpy as np
 import pandas as pd
 import xgboost as xgb
+from sklearn.decomposition import PCA
 from sklearn.preprocessing import OneHotEncoder
 
-df = pd.read_csv("train.csv")
-df.drop("Id", axis=1, inplace=True)
+df = pd.read_csv("train.csv", index_col="Id")
 data = pd.get_dummies(df)
-X, Y = data.iloc[:,1:].values, data["Score"].values
+X, Y = data.iloc[:,17:].values, data["Score"].values
 n = int(X.shape[0] * 0.8)
+
+xx = data.iloc[:,1:17].values
+pca = PCA(10)
+xx = pca.fit_transform(xx)
+print(pca.explained_variance_ratio_)
+print(sum(pca.explained_variance_ratio_))
+print(xx.shape, X.shape)
+X = np.hstack((xx, X))
+print(X.shape)
 
 dtrain = xgb.DMatrix(X[:n], label=Y[:n])
 dtest = xgb.DMatrix(X[n:], label=Y[n:])
@@ -18,6 +28,6 @@ param = {'objective': 'reg:linear',
         'silent': 1,
         'nthread': 4,
         'save_period': 0}
-num_round = 1110
+num_round = 1000
 watchlist = [(dtest, 'eval'), (dtrain, 'train')]
 bst = xgb.train(param, dtrain, num_round, watchlist)
